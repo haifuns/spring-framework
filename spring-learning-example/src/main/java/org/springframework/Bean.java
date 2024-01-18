3ebf02b7ff1368a6e1ea58077f8505d31d128414
haifuns <haifuns@163.com>
2024-01-18 23:18:47 +0800

    提交实例项目，运行框架源码

---
+++ spring-learning-example/src/main/java/org/springframework/Bean.java
@@ -0,0 +1,8 @@
+package org.springframework;
+
+/**
+ * @author haifuns
+ * @date 2024/1/18 23:05
+ */
+public class Bean {
+}
---
+++ spring-learning-example/src/main/java/org/springframework/Main.java
@@ -0,0 +1,17 @@
+package org.springframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author haifuns
+ * @date 2024/1/18 22:52
+ */
+public class Main {
+	public static void main(String[] args) {
+		// 运行报错，core/context kotlin类找不到时，到对应项目test/java/example文件夹右键执行单元测试即可
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.register(Bean.class);
+		context.refresh();
+		System.out.println(context.getBean(Bean.class));
+	}
+}
