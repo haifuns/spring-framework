1dcd601a36c31821b854c39f65f8b1a779e02b4e
haifuns <haifuns@163.com>
2024-10-23 22:24:12 +0800

    invokeBeanFactoryPostProcessor

--- spring-learning-example/src/test/java/com/test/class0501_context/ContextTest.java
+++ spring-learning-example/src/test/java/com/test/class0501_context/ContextTest.java
@@ -11,6 +11,39 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @date 2024-10-16 17:16
  */
 public class ContextTest {
+
+	@Test
+	public void testBeanDefinitionMap() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ContextConfig.class);
+		context.refresh();
+
+		// spring中启动自动加载到beanDefinitionMap中的类，加载位置：
+		// AnnotationConfigApplicationContext构造方法->
+		// AnnotatedBeanDefinitionReader构造方法->
+		// AnnotationConfigUtils#registerAnnotationConfigProcessors
+
+		// 自带注册的5个类：
+		// ConfigurationClassPostProcessor
+		// AutowiredAnnotationBeanPostProcessor 解析@Autowired
+		// CommonAnnotationBeanPostProcessor 解析@Resource
+		// EventListenerMethodProcessor
+		// DefaultEventListenerFactory
+
+		// BeanDefinitionMap中存储扫描或内置的BeanDefinition信息，name -> BeanDefinition
+	}
+
+	@Test
+	public void invokeBeanFactoryPostProcessor() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.test.class0501_context");
+		context.addBeanFactoryPostProcessor(new ManualBeanFactoryPostProcessor());
+		context.refresh();
+		
+		// BeanFactoryPostProcessor
+		// BeanDefinitionRegistryPostProcessor extends BeanFactoryPostProcessor
+	}
+
+
 	@Test
 	public void testBeanDefinitionRegistryOrder() {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
