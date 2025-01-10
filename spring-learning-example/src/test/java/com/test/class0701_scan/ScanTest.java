0780fbf566d770f9d3c9fde867161f5a8b59c892
haifuns <haifuns@163.com>
2025-01-10 19:21:52 +0800

    内置扫描器

--- spring-learning-example/src/test/java/com/test/class0701_scan/ScanTest.java
+++ spring-learning-example/src/test/java/com/test/class0701_scan/ScanTest.java
@@ -23,4 +23,23 @@ public class ScanTest {
 		context.register(ScanBeanDefinitionRegistryPostProcessor.class);
 		context.refresh();
 	}
+
+	@Test
+	public void twoScanner() {
+		// Spring内置两个扫描器
+		// 1. AnnotationConfigApplicationContext构造方法创建，用于处理用户手动调用#scan扫描
+		// 2. ConfigurationClassPostProcessor#postProcessBeanDefinitionRegistry创建，用于处理@ComponentScan，功能更丰富
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.test.class0701_scan");
+
+		// Spring扫描流程：
+		// 1. 实例化、初始化扫描器，初始化的时候默认注册了三个include过滤器
+		// 2. 解析@ComponentScan注解得到包名
+		// 3. 根据包名扫描所有的文件
+		// 4. 遍历文件，通过ASM字节码技术读取文件信息，封装为MetadataReader对象
+		// 5. 调用#isCandidateComponent，通过MetadataReader判断是否exclude、是否include
+		// 6. 如果被include则实例化ScannedGenericBeanDeﬁnition对象
+		// 7. 判断ScannedGenericBeanDeﬁnition是否接口是否抽象、是否加了LockUp注解等
+		// 8. 如果正常则put到beanDeﬁnitionMap
+	}
 }
